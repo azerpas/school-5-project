bb3b905b6bd612c777b18d71d18d89a2ac9d70ae
williamchnt <56387759+williamchnt@users.noreply.github.com>
2020-12-29 14:03:51 +0100

    V1 William

---
+++ Test.m
@@ -0,0 +1,12 @@
+function Fruits = Test(pathToTest,net) 
+    imds = imageDatastore(pathToTest, 'IncludeSubfolders', true, 'LabelSource', "foldernames");
+    auds = augmentedImageDatastore([227,227], imds); % Resize to 227 227
+    preds = classify(net,auds);
+    disp(preds);
+    
+    numCorrect = nnz(imds.Labels == preds);
+    fracCorrect = numCorrect/numel(preds);
+    disp(fracCorrect); % Number of valid results
+    confusionchart(imds.Labels, preds);
+
+end
