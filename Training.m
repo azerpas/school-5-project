00d2bc99ddb5f3f58fe42765f96dd79f72e0acaa
azerpas <xskylix@hotmail.fr>
2021-01-02 14:35:20 +0100

    Réécris Training.m

--- Training.m
+++ Training.m
@@ -1,24 +1,43 @@
-function TrainFruits = Training(pathToTrain)
+function TrainedNet = Training(pathToTrain)
+    % -------------------------
+    % Set up training data
+    % -------------------------
+    imdsTrain = imageDatastore(pathToTrain, 'IncludeSubfolders', true, 'LabelSource', "foldernames");
+    nbCategories = numel(categories(imdsTrain.Labels));
     % -------------------------
     % Create new layers to train
     % -------------------------
     net = alexnet;
     layers = net.Layers;
-    layers(end-2) = fullyConnectedLayer(4); % 131 nb de classes
-    layers(end) = classificationLayer;
+    layersTransfer = layers(1:end-3);
+    layersTransfer(end+1) = fullyConnectedLayer(nbCategories, 'WeightLearnRateFactor', 20, 'BiasLearnRateFactor', 20);
+    layersTransfer(end+1) = softmaxLayer();
+    layersTransfer(end+1) = classificationLayer();
     % -------------------------
-    % Set up training data
+    % Modify images to fit network
     % -------------------------
-    imds = imageDatastore(pathToTrain, 'IncludeSubfolders', true, 'LabelSource', "foldernames");
-    disp(imds.Labels);
+    pixelRange = [-30 30];
+    imageAugmenter = imageDataAugmenter( ...
+        'RandXReflection',true, ...
+        'RandXTranslation',pixelRange, ...
+        'RandYTranslation',pixelRange);
+    augimdsTrain = augmentedImageDatastore([227,227],imdsTrain, ...
+        'DataAugmentation',imageAugmenter);
     % -------------------------
     % Train network with Training data
     % -------------------------
-    options = trainingOptions('sgdm','InitialLearnRate', 0.001);
-    auds = augmentedImageDatastore([227,227], imds); % Resize to 227 227
-    [fruitnet,info] = trainNetwork(auds, layers, options);
-    disp(info)
+    options = trainingOptions('sgdm', ...
+        'LearnRateSchedule','piecewise', ...
+        'LearnRateDropFactor',0.2, ...
+        'LearnRateDropPeriod',5, ...
+        'MaxEpochs',20, ...
+        'MiniBatchSize',64, ...
+        'Verbose',false, ...
+        'ExecutionEnvironment','gpu', % À ENLEVER, si sur CPU
+        'Plots','training-progress');
+    [net,info] = trainNetwork(auds, layers, options);
+    disp(info);
     plot(info.TrainingLoss);
-    
-    TrainFruits = fruitnet;
+    save('./net.mat','net');
+    TrainedNet = net;
 end
