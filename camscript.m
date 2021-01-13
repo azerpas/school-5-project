135cebeb2c7cf12b19ea1bfa37a8e2b4214b5f19
azerpas <xskylix@hotmail.fr>
2021-01-13 23:24:10 +0100

    Modified camscript and added notice to README

--- camscript.m
+++ camscript.m
@@ -1,23 +1,31 @@
 import PackageName.Training.*
 import PackageName.Test.*
 
-cam = ipcam('http://192.168.0.34:8080/video');
-disp("Réseau en cours d'importation, veuillez patienter");
-load('fruitnet.mat');
-disp("Réseau importer");
-preview(cam);
-val = input("appuyez sur la touche '1' Pour reconnaitre l'image\n");
-import PackageName.Test.*
-pathToTest = "smartphonePicture";
-
-while(val~=0)
-    if(val==1)
-        disp('Photo en cours de traitement')
-       image=snapshot(cam);
-       imshow(image); 
-       saveas(imshow(image),'smartphonePicture/imageTaken.png')
-       Test(pathToTest,fruitnet);
+function camera = camScript(urlToCam)
+    cam = ipcam(urlToCam); % 'http://192.168.0.34:8080/video'
+    disp("Importing network...");
+    net = load('net.mat');
+    disp("Network retrieved, accessing the camera...");
+    preview(cam);
+    val = input("Press '1' whenever you're ready to classify the fruit: \n");
+    pathToTest = "smartphonePicture";
+    if ~exist(pathToTest, 'dir')
+        mkdir(pathToTest)
     end
-    val = input("appuyez sur la touche '1' Pour reconnaitre l'image sinon appuyer sur 0\n");
+    
+
+    while(val~=0)
+        if(val==1)
+            disp('Classifying the picture...')
+            image = snapshot(cam);
+            imshow(image); 
+            t = datetime("now","TimeZone",'Europe/Paris');
+            saveas(imshow(image), 'smartphonePicture/imageTaken'+datestr(t, 'ddmmmyyyyHHMMSS')+'.png')
+            Test(pathToTest,net);
+        end
+        disp("Please choose an option:");
+        disp("1. Classify the image");
+        disp("0. Quit the application");
+        val = input("Write a number: \n");
+    end
 end
-disp("Fin du programme")
